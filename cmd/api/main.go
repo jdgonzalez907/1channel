c763630b6e106433de7dc3155d680fa81666294c
Juan David González Bedoya <jdgonzalez907@gmail.com>
2026-09-05 20:26:57 -0500

    feat(api): wire server with router, logger and meta webhooks

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	httprouter "github.com/jdgonzalez907/1channel/internal/http"
+	"github.com/jdgonzalez907/1channel/internal/logger"
 
 	"github.com/jdgonzalez907/1channel/internal/config"
+	"github.com/jdgonzalez907/1channel/internal/meta"
 )
 
 func main() {
 	cfg, err := config.NewConfiguration()
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("%+v\n", cfg)
+		slog.Error("failed to load configuration", "error", err)
+		os.Exit(1)
+	}
+
+	slog.SetDefault(logger.New(cfg))
+
+	router := httprouter.NewRouter()
+	meta.RegisterRoutes(router, cfg)
+
+	addr := ":" + cfg.HTTPPort()
+	slog.Info("server starting", "addr", addr)
+
+	if err := http.ListenAndServe(addr, router); err != nil {
+		slog.Error("server failed", "error", err)
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/jdgonzalez907/1channel
 go 1.27.0
 
 require (
-	github.com/caarlos0/env/v11 v11.4.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/caarlos0/env/v11 v11.4.1
+	github.com/joho/godotenv v1.5.1
 )
